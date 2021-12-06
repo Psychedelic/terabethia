398dc2154fb047cdbc5002ffdec1ffbb5935a06f
BotchM <botchm@hotmail.com>
2021-12-06 12:08:00 +0200

    remove boiler test

--- eth/test/index.ts
+++ eth/test/index.ts
@@ -2,18 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  it("Tera Test", async function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
