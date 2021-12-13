02ccf68d3b70c995a204cdf8cc757824b27ece97
BotchM <botchm@hotmail.com>
2021-12-13 20:46:21 +0200

    upgrade dip20

--- serverless/message.sh
+++ serverless/message.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# curl -X POST https://zebdrmmi5b.execute-api.us-west-2.amazonaws.com/dev/hook -H "Content-Type: application/json" -d "{\"hash\": \"0xf6150e78f9ff55007231f00b86deba1deac71ee328b5aeab14b631c25768fe80\"}"
-curl -X POST http://localhost:3000/dev/message -H "Content-Type: application/json" -d "{\"hash\": \"0xf6150e78f9ff55007231f00b86deba1deac71ee328b5aeab14b631c25768fe80\"}"
+curl -X POST https://zebdrmmi5b.execute-api.us-west-2.amazonaws.com/dev/hook -H "Content-Type: application/json" -d "{\"hash\": \"0x733b835f2b80301f5e4402569cdcff83d2b25b83c01cc9eedd5a8a900016f856\"}"
+# curl -X POST http://localhost:3000/dev/message -H "Content-Type: application/json" -d "{\"hash\": \"0xf9fd3d48648f207858608685c27a714dbf7d7dfc83908d20b2914d710ebdcf90\"}"
