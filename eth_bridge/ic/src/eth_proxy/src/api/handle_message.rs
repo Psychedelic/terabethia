6ec5847a021c4ecf5c017a8307eb603349e390ef
BotchM <botchm@hotmail.com>
2022-05-17 17:38:28 +0300

    rewrite eth_proxy

---
+++ eth_bridge/ic/src/eth_proxy/src/api/handle_message.rs
@@ -0,0 +1,27 @@
+use crate::api::mint::mint;
+use ic_kit::candid::candid_method;
+use ic_kit::{ic, macros::update};
+
+use ic_cdk::export::candid::{Nat, Principal};
+
+use crate::common::types::{EthereumAddr, MagicResponse, Nonce, TokenType, TxError, TxReceipt};
+use crate::proxy::{WETH_ADDRESS_ETH, MAGIC_ADDRESS_IC};
+
+#[update(name = "handle_message")]
+#[candid_method(update, rename = "handle_message")]
+async fn handler(eth_addr: EthereumAddr, nonce: Nonce, payload: Vec<Nat>) -> TxReceipt {
+    let eth_addr_hex = hex::encode(eth_addr);
+
+    if !(eth_addr_hex
+        == WETH_ADDRESS_ETH
+            .trim_start_matches("0x")
+            .to_ascii_lowercase())
+    {
+        return Err(TxError::Other(format!(
+            "Eth Contract Address is inccorrect: {}",
+            eth_addr_hex
+        )));
+    }
+
+    mint(nonce, payload).await
+}
