00962ed4d5643687a7cafddc4b2f7cda41e91740
Federico <macbookpro@MacBooks-MacBook-Pro.local>
2022-08-23 13:18:13 +0200

    add query for authorized controllers

--- eth_bridge/ic/src/eth_proxy/src/api/admin.rs
+++ eth_bridge/ic/src/eth_proxy/src/api/admin.rs
@@ -1,5 +1,5 @@
 use crate::proxy::STATE;
-use ic_kit::{candid::candid_method, macros::update, Principal};
+use ic_kit::{candid::candid_method, macros::query, macros::update, Principal};
 
 pub fn is_authorized() -> Result<(), String> {
     STATE.with(|s| s.is_authorized())
@@ -9,4 +9,10 @@ pub fn is_authorized() -> Result<(), String> {
 #[candid_method(update)]
 fn authorize(other: Principal) {
     STATE.with(|s| s.authorize(other))
+}
+
+#[query(name = "authorized")]
+#[candid_method(query)]
+fn authorized() -> Vec<Principal> {
+    STATE.with(|s| s.controllers.borrow().clone())
 }
