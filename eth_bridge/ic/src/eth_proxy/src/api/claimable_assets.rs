b2d455e621a19afe11e8adbe81b3d18a7f80a6b9
Federico <macbookpro@MacBooks-MacBook-Pro.local>
2022-09-09 14:18:42 +0200

    add cap as storage service to eth_proxy

--- eth_bridge/ic/src/eth_proxy/src/api/claimable_assets.rs
+++ eth_bridge/ic/src/eth_proxy/src/api/claimable_assets.rs
@@ -1,7 +1,6 @@
-use candid::{candid_method, Nat};
-use ic_cdk_macros::{query, update};
+use candid::candid_method;
+use ic_cdk_macros::query;
 
-use crate::api::admin::is_authorized;
 use crate::{
     common::types::{ClaimableMessage, EthereumAddr},
     proxy::STATE,
@@ -13,8 +12,8 @@ fn get_all(eth_address: EthereumAddr) -> Vec<ClaimableMessage> {
     STATE.with(|s| s.get_claimable_messages(eth_address))
 }
 
-#[update(name = "remove_claimable", guard = "is_authorized")]
-#[candid_method(update, rename = "remove_claimable")]
-fn remove_claimable(eth_address: EthereumAddr, amount: Nat) -> Result<(), String> {
-    STATE.with(|s| s.remove_claimable_message(eth_address, amount.clone()))
+#[query(name = "get_claimable_message_size")]
+#[candid_method(query, rename = "get_claimable_message_size")]
+fn get_claimable_message_size() -> usize {
+    STATE.with(|s| s.get_claimable_messages_queue_size())
 }
